35866d1e8d46510c29bf466746c277f6a5596efd
kendall-budget-lab <joshua.m.kendall@yale.edu>
2025-02-24 12:22:16 -0500

    fiscal year revenue table

--- src/main.R
+++ src/main.R
@@ -91,6 +91,90 @@ sims[1:3] %>%
   )
 
 
+# Build Revenue Tables
+1:3 %>%
+  map(
+    .f = ~ sims[.x] %>%
+      map_dfr(., ~as_tibble(.x)) %>%
+      select(static, year, net_revenue) %>%
+      pivot_wider(names_from = year, values_from = net_revenue) %>%
+      filter(!static) %>%
+      mutate(
+        `2025` = 0
+      ) %>%
+      select(static, `2025`, everything()) %>%
+      pivot_longer(
+        !static,
+        names_to = "year",
+        values_to = "val"
+      )  %>%
+      mutate(
+        fy_val = case_when(
+          year == 2025 ~ 0,
+          .x   != 3    ~ lag(val),
+          T            ~ .75 * val + .25 * lag(val)
+        )
+      ) %>%
+      select(!val) %>%
+      pivot_wider(names_from = year, values_from = fy_val) %>%
+      mutate(
+        scenario = paste0("Option ", .x),
+        budget_window = `2026` + `2027` + `2028` + `2029` + `2030` + `2031` + `2032` 
+        + `2033` + `2034` + `2035`,
+        second_decade = `2036` + `2037` + `2038` + `2039` + `2040` + `2041` + `2042` 
+        + `2043` + `2044` + `2045`,
+        third_decade  = `2046` + `2047` + `2048` + `2049` + `2050` + `2051` + `2052` 
+        + `2053` + `2054` + `2055`
+      ) %>%
+      select(scenario, `2026`, `2027`, `2028`, `2029`, `2030`, `2031`, `2032`,
+             `2033`, `2034`, `2035`,
+             budget_window, second_decade, third_decade)
+  ) %>%
+  bind_rows() %>%
+  pivot_longer(
+    !scenario,
+    names_to = 'year',
+    values_to = 'net_revenue'
+  ) %>%
+  left_join(
+    macro_projections$economic %>%
+      filter(between(year, 2026, 2055)) %>%
+      select(year, gdp) %>%
+      mutate(
+        year = as.character(year)
+      ) %>%
+      pivot_wider(names_from = year, values_from = gdp) %>%
+      mutate(
+        hold = 1,
+        budget_window = `2026` + `2027` + `2028` + `2029` + `2030` + `2031` + `2032` 
+        + `2033` + `2034` + `2035`,
+        second_decade = `2036` + `2037` + `2038` + `2039` + `2040` + `2041` + `2042` 
+        + `2043` + `2044` + `2045`,
+        third_decade = `2046` + `2047` + `2048` + `2049` + `2050` + `2051` + `2052` 
+        + `2053` + `2054` + `2055`
+      )%>%
+      select(hold, `2026`, `2027`, `2028`, `2029`, `2030`, `2031`, `2032`,
+             `2033`, `2034`, `2035`,
+             budget_window, second_decade, third_decade) %>%
+      pivot_longer(!hold, names_to = "year", values_to = "gdp") %>%
+      select(!hold)
+  ) %>%
+  mutate(
+    share_of_gdp = net_revenue / gdp
+  ) %>%
+  select(!gdp) %>%
+  pivot_wider(names_from = year, values_from = c(net_revenue, share_of_gdp)) %>%
+  select(
+    scenario, starts_with("net_revenue"),
+    share_of_gdp_budget_window, share_of_gdp_second_decade, share_of_gdp_third_decade
+  ) %>%
+  rename_with(
+    ~ str_sub(.x, 13, -1),
+    starts_with("net_revenue")
+  ) %>%
+  select(!c(second_decade, third_decade))
+
+
 #------------------------------------
 # Calculate tax rate differentials
 #------------------------------------
@@ -100,6 +184,4 @@ source('./src/etrs.R')
 #------------------------------------
 
 #------------------------------------
-
-
 
